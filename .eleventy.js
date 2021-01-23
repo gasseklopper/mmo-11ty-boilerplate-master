e20afb75f9e43e27c87ceb8f67a57cccaa0c899e
gasseklopper <m-morley@gmx.de>
2021-01-24 00:24:56 +0100

    sfa

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,28 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("admin");
+	// Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+	return {
+		templateFormats: ["md", "njk", "html", "liquid"],
+
+		// If your site lives in a different subdirectory, change this.
+		// Leading or trailing slashes are all normalized away, so don’t worry about it.
+		// If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+		// This is only used for URLs (it does not affect your file structure)
+		pathPrefix: "/",
+
+		markdownTemplateEngine: "liquid",
+		htmlTemplateEngine: "njk",
+		dataTemplateEngine: "njk",
+		dir: {
+			input: ".",
+			includes: "_includes",
+			data: "_data",
+			output: "_site"
+		}
+	};
 };
